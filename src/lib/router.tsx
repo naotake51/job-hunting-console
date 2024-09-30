8f5afa111a95d01f3408c3b3accc3c987a672932
n-takeshita <naokisyuu@gmail.com>
2024-09-30 23:44:46 +0900

    add: react-router-dom

---
+++ src/lib/router.tsx
@@ -0,0 +1,10 @@
+import App from '@/App.tsx'
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+  },
+]);
